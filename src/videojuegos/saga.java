7e1943dda7dc164ede998beeec70019cde9ff0fa
stefandrea <50913331+stefandrea@users.noreply.github.com>
2019-10-23 15:17:19 -0500

    version 2.0

--- src/videojuegos/saga.java
+++ src/videojuegos/saga.java
@@ -6,15 +6,46 @@ public class saga extends Videojuegos{
     private boolean ingles;
     private String autor; 
 
-    public saga(String usuarioDelJugador, int niveles, int cantidadDeAvatares, int inventario, boolean online, boolean actualizaciones) {
-        super(usuarioDelJugador, niveles, cantidadDeAvatares, inventario, online, actualizaciones);
-    }
-
     public saga(int episodios, boolean ingles, String autor, String usuarioDelJugador, int niveles, int cantidadDeAvatares, int inventario, boolean online, boolean actualizaciones) {
         super(usuarioDelJugador, niveles, cantidadDeAvatares, inventario, online, actualizaciones);
         this.episodios = episodios;
         this.ingles = ingles;
         this.autor = autor;
     }
+
+    public int getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public boolean isIngles() {
+        return ingles;
+    }
+
+    public void setIngles(boolean ingles) {
+        this.ingles = ingles;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+   public void episodios(int nuevosEpisodios){
+        if(nuevosEpisodios<=this.episodios){
+            System.out.println("no hay nuevos episodios: ");
+            
+        }else{
+            this.episodios = nuevosEpisodios;
+            System.out.println("Los nuevos episodios son: ");
+        }
+    }
+    
  
 }
